95266fddcc8965fbee818f58ebbdb67d2b983f11
Landon Carter <lcarter@mit.edu>
2016-10-02 16:43:25 -0400

    blob generation

--- labs/lab3/lab3.v
+++ labs/lab3/lab3.v
@@ -459,6 +459,29 @@ endmodule
 
 ////////////////////////////////////////////////////////////////////////////////
 //
+// blob: generate rectangle on screen
+//
+////////////////////////////////////////////////////////////////////////////////
+
+module blob
+   #(parameter WIDTH = 64,             // default width: 64 pixels
+     parameter HEIGHT = 64,            // default height: 64 pixels
+     parameter COLOR = 24'hFF_FF_FF);  // default color: white
+   (input [10:0] x,hcount,
+    input [9:0] y, vcount,
+    output reg [23:0] pixel);
+
+   always @(*) begin
+      if ((hcount >= x && hcount < (x+WIDTH)) &&
+          (vcount >= y && vcount < (y+HEIGHT)))
+         pixel = COLOR;
+      else pixel = 0;
+   end
+endmodule
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
 // pong_game: the game itself!
 //
 ////////////////////////////////////////////////////////////////////////////////
